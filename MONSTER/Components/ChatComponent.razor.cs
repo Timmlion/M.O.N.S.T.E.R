d40dd674260c5aebf49628dee79afbe37821012c
Adam Siwek <timmlion.istiris@gmail.com>
2023-05-03 00:49:53 +0200

    Initial main layout

---
+++ MONSTER/Components/ChatComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MONSTER.Components
+{
+    public partial class ChatComponent : ComponentBase
+    {
+        private List<ChatElement> _chatElements = new List<ChatElement> {
+            new ChatElement {Content="What is lorem ipsum?",Variant = Variant.Text, HorizontalAlignment=HorizontalAlignment.Right },{
+            new ChatElement {Content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                                       Variant = Variant.Outlined, HorizontalAlignment=HorizontalAlignment.Left} } };
+
+        private string _chatInput;
+        
+    }
+
+    public class ChatElement
+    {
+        public string Content { get; set; }
+        public Variant Variant { get; set; }
+        public HorizontalAlignment HorizontalAlignment{get;set;}
+    }
+}
---
+++ MONSTER/Components/ModelsListComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MONSTER.Components
+{
+    public partial class ModelsListComponent : ComponentBase
+    {
+        private List<AssistantModel> _assistantModels = new List<AssistantModel> { new AssistantModel { Name="Tłumacz", SystemPrompt="Trolo", Icon = Icons.Material.Filled.LibraryBooks, Variant = Variant.Outlined },
+                                                                                                             new AssistantModel { Name="CoPilot", SystemPrompt="Trolo2", Icon = Icons.Material.Filled.Computer, Variant = Variant.Outlined}};
+        private void SetPrompt(AssistantModel am)
+        {
+            ResetVariant();
+            am.Variant = Variant.Filled;
+            StateHasChanged();
+        }
+
+        private void ResetVariant()
+        {
+            foreach(var assistant in _assistantModels)
+            {
+                assistant.Variant = Variant.Outlined;
+            }
+        }
+    }
+
+    public class AssistantModel
+    {
+        public string Name { get; set; }
+        public string SystemPrompt { get; set; }
+        public string Icon { get; set; }
+        public Variant Variant { get; set; }
+    }
+}
